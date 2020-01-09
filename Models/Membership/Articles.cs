d521e8a9d31b874355f6d7bcecfbb343b575cfd4
FauzanAr <faurinta12345@gmail.com>
2020-01-09 15:23:56 +0800

    Creating articles model

--- Models/Membership/Articles.cs
+++ Models/Membership/Articles.cs
@@ -4,8 +4,21 @@ using Newtonsoft.Json;
 
 namespace membership_api.Models
 {
-    public class Articles
+    public partial class Articles
     {
+         public int ArticlesId { get; set; }
+         public String ArticlesTitle { get; set; }
+         public String ArticlesContent { get; set; }
+         public String ArticlesAuthorName { get; set; }
+         public DateTime ArticlesCreatedAt { get; set; }
+         public String ArticlesCreatedByUsersId { get; set; }
+         public String ArticlesCreatedByUsersName { get; set; }
+         public DateTime ArticlesUpdatedAt { get; set; }
+         public String ArticlesUpdatedByUsersId { get; set; }
+         public String ArticlesUpdatedByUsersName { get; set; }
+         public DateTime? ArticlesDeletedAt { get; set; }
+         public String ArticlesDeletedByUsersId { get; set; }
+         public String ArticlesDeletedByUsersName { get; set; }
          
     }
 }
