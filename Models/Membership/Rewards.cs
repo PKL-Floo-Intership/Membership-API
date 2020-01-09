9b9119577797f9fef1e761cb150ffa1217e286bb
FauzanAr <faurinta12345@gmail.com>
2020-01-09 17:06:07 +0800

    Adding rewards model

---
+++ Models/Membership/Rewards.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace membership_api
+{
+    public partial class Rewards
+    {
+        public int RewardsId { get; set; }
+        public String RewardsName { get; set; }
+        public String RewardsDescription { get; set; }
+        public String RewardsIcon { get; set; }
+        public DateTime RewardsCreatedAt { get; set; }
+        public String RewardsCreatedByUsersId { get; set; }
+        public String RewardsCreatedByUsersName { get; set; }
+        public DateTime RewardsUpdatedAt { get; set; }
+        public String RewardsUpdatedByUsersId { get; set; }
+        public String RewardsUpdatedByUsersName { get; set; }
+        public DateTime? RewardsDeletedAt { get; set; }
+        public String RewardsDeletedByUsersId { get; set; }
+        public String RewardsDeletedByUsersName { get; set; }
+        public Boolean RewardsIsActive { get; set; }
+    }
+}
