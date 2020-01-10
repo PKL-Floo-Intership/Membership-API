e953b11b0d1c9f4f2b780242b6b5c8e940c901a3
FauzanAr <faurinta12345@gmail.com>
2020-01-10 10:09:18 +0800

    model builder for articles entity done

--- Models/Membership/MembershipContext.cs
+++ Models/Membership/MembershipContext.cs
@@ -17,6 +17,73 @@ namespace membership_api.Models
         public virtual DbSet<UserPoints> UserPoints { get; set; }
         public virtual DbSet<UserRewards> UserRewards { get; set; }
         public virtual DbSet<Users> Users { get; set; }
-        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=FAUZANAR-PC,FAUZAN;Database=membership_floo;Integrated Security=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.HasAnnotation("ProductVersion","2.2.6-servicing-10079");
+            modelBuilder.Entity<Articles>(entity =>
+            {
+                entity.ToTable("ARTICLES");
+
+                entity.Property(e => e.ArticlesId)
+                    .IsRequired()
+                    .HasColumnName("ARTICLES_ID")
+                    .ValueGeneratedOnAdd();
+                entity.Property(e => e.ArticlesTitle)
+                    .IsRequired()
+                    .HasColumnName("ARTICLES_TITLE")
+                    .IsUnicode(false)
+                    .HasMaxLength(16);
+                entity.Property(e => e.ArticlesContent)
+                    .IsRequired()
+                    .HasColumnName("ARTICLES_CONTENT")
+                    .HasColumnType("text");
+                entity.Property(e => e.ArticlesAuthorName)
+                    .IsRequired()
+                    .HasColumnName("ARTICLES_AUTHOR_NAME")
+                    .IsUnicode(false)
+                    .HasMaxLength(15);
+                entity.Property(e => e.ArticlesCreatedAt)
+                    .HasColumnName("ARTICLES_CREATED_AT")
+                    .HasColumnType("datetime");
+                entity.Property(e => e.ArticlesCreatedByUsersId)
+                    .HasColumnName("ARTICLES_CREATED_BY_USERS_ID")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+                entity.Property(e => e.ArticlesCreatedByUsersName)
+                    .HasColumnName("ARTICLES_CREATED_BY_USERS_NAME")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+                entity.Property(e => e.ArticlesUpdatedAt)
+                    .HasColumnName("ARTICLES_UPDATED_AT")
+                    .HasColumnType("datetime");
+                entity.Property(e => e.ArticlesUpdatedByUsersId)
+                    .HasColumnName("ARTICLES_UPDATED_BY_USERS_ID")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+                entity.Property(e => e.ArticlesUpdatedByUsersName)
+                    .HasColumnName("ARTICLES_UPDATED_BY_USERS_NAME")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+                entity.Property(e => e.ArticlesDeletedAt)
+                    .HasColumnName("ARTICLES_UPDATED_AT")
+                    .HasColumnType("datetime");
+                entity.Property(e => e.ArticlesDeletedByUsersId)
+                    .HasColumnName("ARTICLES_DELETED_BY_USERS_ID")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+                entity.Property(e => e.ArticlesDeletedByUsersName)
+                    .HasColumnName("ARTICLES_DELETED_BY_USERS_NAME")
+                    .IsUnicode(false)
+                    .HasMaxLength(50);
+            });
+        }
     }
 }
